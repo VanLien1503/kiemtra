5b438026cbf06b7901e737661fe0ddbf7f66696a
VanLien1503 <hoanglien89nd@gmail.com>
2020-12-17 09:00:04 +0700

    Initial commit

---
+++ src/main/java/code/gym/com/Md4KtApplication.java
@@ -0,0 +1,13 @@
+package code.gym.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Md4KtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Md4KtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/code/gym/com/Md4KtApplicationTests.java
@@ -0,0 +1,13 @@
+package code.gym.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Md4KtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
